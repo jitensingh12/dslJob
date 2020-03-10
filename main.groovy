e337813527ec15db5c12cc238625f64204142b24
root <root@ip-10-10-4-211.ec2.internal>
2020-03-10 08:15:58 +0000

    ff

--- main.groovy
+++ main.groovy
@@ -5,7 +5,7 @@
              git {
 		remote {
 			//github('jitensingh12/jenkinfilejob.git', 'https')
-			git branch: 'master', changelog: false, credentialsId: 'b0f25a4f-9107-411b-85b2-ad7d4d89a6d4', poll: false, url: 'https://github.com/jitensingh12/jenkinfilejob.git'
+			git branch: 'master', credentialsId: 'b0f25a4f-9107-411b-85b2-ad7d4d89a6d4', url: 'https://github.com/jitensingh12/jenkinfilejob.git'
 		}
 		branch('master')
 	    }
