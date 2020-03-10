f7ebb8c79d0fbfbc62098b8a048399dbef13680f
root <root@ip-10-10-4-211.ec2.internal>
2020-03-10 08:37:22 +0000

    g

--- main.groovy
+++ main.groovy
@@ -4,10 +4,11 @@
 	scm {
              git {
 		remote {
-			//github('jitensingh12/jenkinfilejob.git', 'https')
-			git branch: 'master', credentialsId: 'b0f25a4f-9107-411b-85b2-ad7d4d89a6d4', url: 'https://github.com/jitensingh12/jenkinfilejob.git'
+			github('jitensingh12/jenkinfilejob.git', 'https')
+			//git branch: 'master', credentialsId: 'b0f25a4f-9107-411b-85b2-ad7d4d89a6d4', url: 'https://github.com/jitensingh12/jenkinfilejob.git'
 		}
 		branch('master')
+		scanCredentialsId('b0f25a4f-9107-411b-85b2-ad7d4d89a6d4')
 	    }
 	}
 	steps {
